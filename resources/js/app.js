73eda9fc117681ce6a0da7de24329957c91134a2
FaizAlfaRizqy <muhammadumarfaiz18@gmail.com>
2026-07-17 23:51:35 +0700

    add timer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("timer-value").textContent = "Halo";
-});
+let elapsedSeconds = 0;
+let minutes = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("timer-value").textContent = minutes + ":" + elapsedSeconds;
+    });
+
+function updateTimer() {
+    elapsedSeconds++;
+    if (elapsedSeconds >= 60) {
+        elapsedSeconds = 0;
+        minutes++;
+    }
+    document.getElementById("timer-value").textContent = (minutes < 10 ? "0" : "") + minutes + ":" + (elapsedSeconds < 10 ? "0" : "") + elapsedSeconds;
+}
+
+setInterval(updateTimer, 1000);
